a30e2495407097d3821a7665f19d9ae613eae2c8
Emmanuel-dot-spec <79093920+Emmanuel-dot-spec@users.noreply.github.com>
2021-05-26 15:59:22 +0100

    first commit

---
+++ process.js
@@ -0,0 +1,19 @@
+// << ? php
+
+// Connect to a database
+
+// console.log('processing...');
+
+// CHECK FOR POST VARIABLE
+// if (isset($_POST['name'])) {
+//     console.log('POST: Your name is '.$_POST['name']);
+// }
+
+// CHECK FOR GET VARIABLE
+// if (isset($_GET['name'])) {
+//     console.log('GET: Your name is '.$_GET['name']);
+// }
+
+if (typeof obj.foo !== 'undefined') {
+    console.log('GET: Your name is '.$_GET['name']);
+}
